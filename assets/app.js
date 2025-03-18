fe8bc02e631b83ccac534c86f14d3b4fe75851e1
HervillardLeo <hervillard.leo@gmail.com>
2025-03-18 21:16:38 +0100

    feat: :sparkles: Step 1 : client-specific car display

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,38 @@
-import './styles/app.css';
+import "./styles/app.css";
 
-import $ from 'jquery';
+import $ from "jquery";
 
-$(function() {
-    console.log("jquery loaded");
+$(function () {
+  loadContent();
+  $("#client-selector").on("change", (e) => changeClient(e.target.value));
+});
+
+function loadContent() {
+  var module = $(".dynamic-div").data("module");
+  var script = $(".dynamic-div").data("script");
+
+  fetch("/load-content", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({ module: module, script: script }),
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      $(".dynamic-div").innerHTML = data;
+    })
+    .catch((error) => console.error("Erreur de chargement :", error));
+}
+
+function changeClient(clientId) {
+  fetch("/change-client", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({ client: clientId }),
+  }).then(() => {
+    loadContent();
   });
-
+}
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,16 +3,49 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Cookie;
 
 final class IndexController extends AbstractController
 {
     #[Route('/index', name: 'app_index')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        // Récupérer le client depuis le cookie (par défaut : clienta)
+        $client = $request->cookies->get('client_id', 'clienta');
+
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'client' => $client
         ]);
+    }
+
+    #[Route('/change-client', name: 'change_client', methods: ['POST'])]
+    public function changeClient(Request $request): JsonResponse
+    {
+        $client = $request->request->get('client', 'clienta');
+
+        $response = new JsonResponse(['message' => 'Client changé']);
+        $response->headers->setCookie(new Cookie('client_id', $client, strtotime('+1 day')));
+
+        return $response;
+    }
+
+    #[Route('/load-content', name: 'load_content', methods: ['POST'])]
+    public function loadContent(Request $request): Response
+    {
+        $client = $request->cookies->get('client_id', 'clienta');
+        $module = $request->request->get('module', 'cars');
+        $script = $request->request->get('script', 'ajax');
+
+        $templatePath = "customs/$client/modules/$module/$script.html.twig";
+
+        if ($this->container->get('twig')->getLoader()->exists($templatePath)) {
+            return $this->render($templatePath);
+        }
+
+        return new Response("<p>Fichier introuvable : $templatePath</p>", Response::HTTP_NOT_FOUND);
     }
 }
